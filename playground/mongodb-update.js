b9e66e79aed6437476b1aa1078ce34aa0e34f8b4
jeremyswann <mail@jeremyswann.com>
2018-09-06 13:58:36 +1000

    :sparkles: update data

---
+++ playground/mongodb-update.js
@@ -0,0 +1,33 @@
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect(
+	'mongodb://localhost:27017/TodoApp',
+	{ useNewUrlParser: true },
+	(err, client) => {
+		if (err) {
+			return console.log('Unable to connect to MongoDB server')
+		}
+		console.log('Connected to MongoDB server')
+		const db = client.db('TodoApp')
+
+		// * findOneAndUpdate
+		db.collection('Todos')
+			.findOneAndUpdate(
+				{ _id: new ObjectID('5b909befda403d3a3007fae5') },
+				{ $set: { completed: true } },
+				{ returnOriginal: false }
+			)
+			.then(res => console.log(res))
+
+		// * findOneAndUpdate
+		db.collection('Users')
+			.findOneAndUpdate(
+				{ _id: new ObjectID('5b908c930758b710a4eac652') },
+				{ $inc: { age: +1 } },
+				{ returnOriginal: false }
+			)
+			.then(res => console.log(res))
+
+		client.close()
+	}
+)
